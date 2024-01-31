f2f9039efecd14ea3d46dc468b57b294dc8d46da
Slinkter <luis.j.cueva@gmail.com>
2024-01-31 13:30:49 -0500

    updae

--- src/App.jsx
+++ src/App.jsx
@@ -5,62 +5,66 @@ import { Input } from "@material-tailwind/react";
 const api_url = "https://jsonplaceholder.typicode.com/users";
 
 const App = () => {
+  const [originalUsers, setOriginalUsers] = useState([]);
   const [users, setUsers] = useState([]);
-  const [filterListUser, setFilterListUser] = useState([]);
+  const [loading, setLoading] = useState(true);
   const [textInput, setTextInput] = useState("");
-  //
-  const [selected, setSelected] = React.useState(1);
-  const setSelectedItem = (value) => setSelected(value);
 
-  //
   const getUsers = async () => {
-    const res = await fetch(api_url);
-    const data = await res.json();
-    setUsers(data);
+    try {
+      const res = await fetch(api_url);
+      const data = await res.json();
+      setOriginalUsers(data);
+      setUsers(data);
+      setLoading(false);
+    } catch (error) {
+      console.error("Error fetching data from API:", error);
+    }
   };
 
   useEffect(() => {
     getUsers();
-    return () => {};
   }, []);
-  //
-  const listfilted = () =>
-    users.filter((user) =>
+  /* parte 2 :  */
+  const handleSearch = () => {
+    const filteredUsers = originalUsers.filter((user) =>
       Object.values(user).join().toLowerCase().includes(textInput.toLowerCase())
     );
-  //
+    console.log("filter  :", filteredUsers);
+    setUsers(filteredUsers);
+  };
+
   useEffect(() => {
-    if (!textInput) {
-      setFilterListUser(users);
-    } else {
-      setFilterListUser(listfilted());
+    handleSearch();
+  }, [textInput, originalUsers]);
+
+  /* parte 3 : Validacion  */
+  const renderUsers = () => {
+    if (loading) {
+      return <div className="p-5">Loading....</div>;
     }
-  }, [textInput]);
-  //
-  const textEmpty = filterListUser.map((user) => (
-    <List key={user.id}>
-      <ComponentRenderUser usuario={user} />
-    </List>
-  ));
+    if (users.length === 0) {
+      return <div className="p-5">no hay datos....</div>;
+    }
+    return (
+      <List>
+        {users.map((user) => (
+          <ComponentRenderUser usuario={user} key={user.id} />
+        ))}
+      </List>
+    );
+  };
 
-  const textFill = users.map((user) => (
-    <List key={user.id}>
-      <ComponentRenderUser usuario={user} />
-    </List>
-  ));
-
-  //
-  console.log(users);
   return (
-    <div className="flex flex-col justify-center items-center  min-h-dvh w-screen   sm:bg-yellow-300 md:bg-red-300  bg-gray-300 ">
-      <div className=" w-3/4 sm:w-3/4  md:w-2/4   h-full m-5">
+    <div className="flex flex-col justify-center items-center min-h-dvh w-screen sm:bg-yellow-300 md:bg-blue-gray-600 bg-blue-gray-600">
+      <div className="w-3/4 sm:w-3/4 md:w-2/4 h-full m-5">
         <Input
           label="nombre"
           color="white"
           onChange={(e) => setTextInput(e.target.value)}
         />
         <br />
-        <Card className=" ">{textInput > 0 ? textFill : textEmpty}</Card>
+        <Card>{renderUsers()}</Card>
       </div>
     </div>
   );
@@ -69,7 +73,7 @@ const App = () => {
 const ComponentRenderUser = ({ usuario }) => {
   return (
     <div>
-      <ListItem className="flex flex-col justify-center items-center ">
+      <ListItem className="flex flex-col justify-center items-center">
         <h3>{usuario.name}</h3>
         <p>{usuario.email}</p>
       </ListItem>
